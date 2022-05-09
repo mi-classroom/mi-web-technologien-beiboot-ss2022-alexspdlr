748f8c60aad89ae718b87376f6c542be2d7d4e4b
alexspdlr <42113852+alexspdlr@users.noreply.github.com>
2022-05-09 20:50:42 +0200

    dynamic port

--- express/server.js
+++ express/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const basicAuth = require("express-basic-auth");
 const path = require("path");
 const app = express();
+const developmentPort = 9000; 
+
 
 app.use(
 	basicAuth({
@@ -19,7 +21,15 @@ app.use((req, res, next) => {
 	res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
+
+// start express server on port 9000
+app.listen(process.env.PORT || developmentPort, () => {
+	console.log(`server started on port ${process.env.PORT || developmentPort}`);
+});
+
+/*
 // start express server on port 9000
 app.listen(9000, () => {
 	console.log("server started on port 9000");
 });
+*/
