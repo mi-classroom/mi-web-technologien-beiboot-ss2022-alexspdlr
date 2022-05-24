47df56160890aa4e53dbf44a36dc5b03c4540dde
alexspdlr <42113852+alexspdlr@users.noreply.github.com>
2022-05-24 17:08:17 +0200

    🔥 remove unused files & code

--- express/server.js
+++ express/server.js
@@ -1,34 +1,23 @@
-
-const express = require("express");
-const basicAuth = require("express-basic-auth");
-const path = require("path");
+const express = require('express');
+const basicAuth = require('express-basic-auth');
+const path = require('path');
 const app = express();
-const developmentPort = 9000; 
-
+const developmentPort = 9000;
 
 app.use(
-	basicAuth({
-		challenge: true,
-		users: { cranach: "meisterwerke" },
-	})
+  basicAuth({
+    challenge: true,
+    users: { cranach: 'meisterwerke' },
+  })
 );
 
-app.use(express.static(path.join(__dirname, "..", "build")));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static('public'));
 
-// add middlewares
 app.use((req, res, next) => {
-	res.sendFile(path.join(__dirname, "..", "build", "index.html"));
-});
- 
-// start express server on port 9000
-app.listen(process.env.PORT || developmentPort, () => {
-	console.log(`Server started on port ${process.env.PORT || developmentPort}`);
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
-/*
-// start express server on port 9000
-app.listen(9000, () => {
-	console.log("server started on port 9000");
+app.listen(process.env.PORT || developmentPort, () => {
+  console.log(`Server started on port ${process.env.PORT || developmentPort}`);
 });
-*/
--- src/App.js
+++ src/App.js
@@ -1,95 +1,94 @@
 import './App.scss';
-import './assets/styles/scss/abstracts/variables.scss'
+import './assets/styles/scss/abstracts/variables.scss';
 import { useState } from 'react';
 
 function App() {
+  const [bestOfs, setBestOfs] = useState(null);
+  const [loading, setLoading] = useState(false);
 
- const [bestOfs, setBestOfs] = useState(null)
- const [loading, setLoading] = useState(false)
+  const onFileChange = (event) => {
+    setLoading(true);
 
- const onFileChange = event => { 
+    var files = event.target.files;
 
-  setLoading(true)
+    if (files.length <= 0) {
+      return false;
+    }
 
-  var files = event.target.files
+    files[0].text().then(function (text) {
+      var result = JSON.parse(text);
+      const filteredItems = result.items.filter(
+        (item) => item.isBestOf === true
+      );
+      const filteredItemsSorted = filteredItems.sort((a, b) =>
+        a.sortingNumber > b.sortingNumber
+          ? 1
+          : b.sortingNumber > a.sortingNumber
+          ? -1
+          : 0
+      );
+      setBestOfs(filteredItemsSorted);
+      console.log(filteredItemsSorted);
+      setLoading(false);
+    });
+  };
 
-  if (files.length <= 0) {
-    return false;
-  }
-
-  files[0].text().then(function(text) { 
-    var result = JSON.parse(text);
-    const filteredItems = result.items.filter(item => item.isBestOf === true)
-    const filteredItemsSorted = filteredItems.sort((a,b) => (a.sortingNumber > b.sortingNumber) ? 1 : ((b.sortingNumber > a.sortingNumber) ? -1 : 0))
-    setBestOfs(filteredItemsSorted)
-    console.log(filteredItemsSorted)
-    setLoading(false)
-  })
-
-}; 
-
-const removeTextInBrackets = (text) => {
-
-  const roundBracketsRemoved = text.split('(')[0]
-
-  const squareBracketsRemoved = roundBracketsRemoved.split('[')[0]
-
-return squareBracketsRemoved
-}
+  const removeTextInBrackets = (text) => {
+    const roundBracketsRemoved = text.split('(')[0];
+    const squareBracketsRemoved = roundBracketsRemoved.split('[')[0];
+    return squareBracketsRemoved;
+  };
 
   return (
-    <div className="App">
-      <div className="App-body" >
-        {bestOfs ? 
-   
-        <div className="listContainer"> 
-        {bestOfs.map(item => 
-        <div className="gridItem" key={item.metadata.id}>
-        <div className="tile" >
-          <div className="previewContainer">
-          <img className="previewImage" src={item.images.overall.images[0].sizes.medium.src} alt={item.metadata.title} />
-          </div> 
-          <div className="h1" >
-          {item.metadata.title}, {item.metadata.date}
+    <div className='App'>
+      <div className='App-body'>
+        {bestOfs ? (
+          <div className='listContainer'>
+            {bestOfs.map((item) => (
+              <div className='gridItem' key={item.metadata.id}>
+                <div className='tile'>
+                  <div className='previewContainer'>
+                    <img
+                      className='previewImage'
+                      src={item.images.overall.images[0].sizes.medium.src}
+                      alt={item.metadata.title}
+                    />
+                  </div>
+                  <div className='h1'>
+                    {item.metadata.title}, {item.metadata.date}
+                  </div>
+                  <div className='h2'>{removeTextInBrackets(item.medium)}</div>
+                  <div className='h2'>{item.repository}</div>
+                </div>
+              </div>
+            ))}
           </div>
-          <div className="h2" > 
-            {removeTextInBrackets(item.medium)}
-          </div>
-          <div className="h2" >
-          {item.repository}
-          </div>
-        </div>
-        </div>)}
-        </div>
+        ) : (
+          <>
+            {loading ? (
+              <p className='h1'>Bitte warten ...</p>
+            ) : (
+              <>
+                <p className='h1'>Lade die benötigte JSON Datei hoch.</p>
 
-        :
-        <>
-        {loading ? 
-        
-        <p className="h1"> 
-        Bitte warten ...
-      </p>
-        :
-        <>
-
-        <p className="h1">  
-          Lade die benötigte JSON Datei hoch.
-        </p>
-
-        {bestOfs ? 
-        <div >{JSON.stringify(bestOfs).slice(0, 10)}</div>
-          :  
-        <div> 
-
-        <label class="uploadButton">
-    <input type="file" onChange={onFileChange} accept="application/JSON" /> 
-    Datei auswählen
-</label>
-
-        </div> }</>
-        
-        }
-        </>}
+                {bestOfs ? (
+                  <div>{JSON.stringify(bestOfs).slice(0, 10)}</div>
+                ) : (
+                  <div>
+                    <label class='uploadButton'>
+                      <input
+                        type='file'
+                        onChange={onFileChange}
+                        accept='application/JSON'
+                      />
+                      Datei auswählen
+                    </label>
+                  </div>
+                )}
+              </>
+            )}
+          </>
+        )}
       </div>
     </div>
   );
